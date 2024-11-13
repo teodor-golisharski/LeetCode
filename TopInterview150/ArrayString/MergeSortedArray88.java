9b2f1b188e03daf8e1859c084627fba764568ae1
Teodor Golisharski <t.golisharski@abv.bg>
2024-11-13 18:35:40 +0200

    MergeSortedArray88 solution added

---
+++ TopInterview150/ArrayString/MergeSortedArray88.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class MergeSortedArray88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        for (int i = m; i < m + n; i++) {
+            nums1[i] = nums2[i-m];
+        }
+        Arrays.sort(nums1);
+    }
+}

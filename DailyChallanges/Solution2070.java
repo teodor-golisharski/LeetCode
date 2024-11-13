ab2b94e7f378f4888232bcd42f14808f057d2bfb
Teodor Golisharski <t.golisharski@abv.bg>
2024-11-13 14:16:39 +0200

    Daily Challenge 2070 solution fixed

--- DailyChallanges/Solution2070.java
+++ DailyChallanges/Solution2070.java
@@ -2,25 +2,40 @@ import java.util.*;
 
 public class Solution2070 {
     public int[] maximumBeauty(int[][] items, int[] queries) {
-        int[] result = new int[queries.length];
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < queries.length; i++) {
+        Arrays.sort(items, (a,b) -> Integer.compare(a[0], b[0]));
 
-            int temp = queries[i];
-            if (map.containsKey(temp)) {
-                result[i] = map.get(temp);
-            } else {
-                int max = Arrays.stream(items)
-                        .filter(item -> item[0] <= temp)
-                        .mapToInt(item -> item[1])
-                        .max()
-                        .orElse(0);
-
-                result[i] = max;
-                map.put(queries[i], max);
-            }
+        int[][] queriesWithIndex = new int[queries.length][2];
+        for (int i = 0; i< queries.length; i++){
+            queriesWithIndex[i][0] = queries[i];
+            queriesWithIndex[i][1] = i;
         }
+        Arrays.sort(queriesWithIndex, Comparator.comparingInt(a -> a[0]));
+        List<int[]> maxBeautyAtPrice = new ArrayList<>();
+        int currentMaxBeauty = 0;
+
+        for (int[] item : items){
+            int price = item[0];
+            int beauty = item[1];
+            currentMaxBeauty = Math.max(currentMaxBeauty, beauty);
+            maxBeautyAtPrice.add(new int[]{price, currentMaxBeauty});
+        }
+
+
+        int[] result = new int[queries.length];
+        int i = 0;
+
+        for (int[] query : queriesWithIndex) {
+            int queryPrice = query[0];
+            int originalIndex = query[1];
+
+            while (i < maxBeautyAtPrice.size() && maxBeautyAtPrice.get(i)[0] <= queryPrice) {
+                i++;
+            }
+
+            result[originalIndex] = (i > 0) ? maxBeautyAtPrice.get(i - 1)[1] : 0;
+        }
+
         return result;
     }
 }

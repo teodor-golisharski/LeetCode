0a00dca17b84e360427776e66cb5f109969cbe5e
Teodor Golisharski <t.golisharski@abv.bg>
2024-11-23 00:33:18 +0200

    JumpGame55 added.

---
+++ TopInterview150/ArrayString/JumpGame55.java
@@ -0,0 +1,13 @@
+public class JumpGame55 {
+    public boolean canJump(int[] nums) {
+        int goal = nums.length - 1;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (i + nums[i] >= goal) {
+                goal = i;
+            }
+        }
+
+        return goal == 0;
+    }
+}

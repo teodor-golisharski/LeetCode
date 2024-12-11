1d0e0a99350a6cdd02f69fa61272a3377a432837
Teodor Golisharski <t.golisharski@abv.bg>
2024-12-11 18:43:05 +0200

    HIndex274 ArrayString solution completed.

--- TopInterview150/ArrayString/HIndex274.java
+++ TopInterview150/ArrayString/HIndex274.java
@@ -1,5 +1,21 @@
+import java.util.Arrays;
+
 public class HIndex274 {
     public int hIndex(int[] citations) {
+        int papers = citations.length;
+        int[] citationBuckets = new int[papers + 1];
 
+        for (int citation : citations) {
+            citationBuckets[Math.min(citation, papers)]++;
+        }
+
+        int cumulativePapers = 0;
+        for (int hIndex = papers; hIndex >= 0; hIndex--) {
+            cumulativePapers += citationBuckets[hIndex];
+            if (cumulativePapers >= hIndex) {
+                return hIndex;
+            }
+        }
+        return 0;
     }
 }
